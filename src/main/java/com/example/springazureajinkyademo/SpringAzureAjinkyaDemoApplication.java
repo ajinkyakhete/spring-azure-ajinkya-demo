1d6a681962fba2d2faff55e201f001db1f12407f
Ajinkya Khete <111764724+ajinkyakhete@users.noreply.github.com>
2024-04-05 00:53:18 +0530

    third commit

--- src/main/java/com/example/springazureajinkyademo/SpringAzureAjinkyaDemoApplication.java
+++ src/main/java/com/example/springazureajinkyademo/SpringAzureAjinkyaDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringAzureAjinkyaDemoApplication {
 	
 	@GetMapping("/message")
 	public String message() {
-		return "Congrats! your application deployed successfully on azure platform !";
+		return "Congrats! Azure your application deployed successfully on azure platform !";
 	}
 
 	public static void main(String[] args) {
